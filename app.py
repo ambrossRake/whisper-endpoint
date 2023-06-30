31dc6bb7ef2553e115d1258fd8fe21e2da6c0f05
Ambross Rake <ijohnson2013301@gmail.com>
2023-06-30 05:15:04 -0700

    Created endpoint

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route('/whisper', methods=['POST']):
+def whisper(request):
+    f = request.files['file']
+    f.save(f.filename)
+    console.log('a')
