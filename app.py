e9f137c32d3f3f4cc8d53ffe106a1bfea8546f9d
Ambross Rake <ijohnson2013301@gmail.com>
2023-07-01 09:56:13 -0700

    endpoint changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods=[, "POST)
+@app.route("/", methods=['POST'])
 def whisper(request):
     f = request.files['file']
     f.save(f.filename)
